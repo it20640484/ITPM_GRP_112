b0e8ea4daa927b1742cb60f270c0dd1403bab282
it20640484 <it20640484@my.sliit.lk>
2023-05-22 11:37:48 +0530

    addAdvertisement update

--- front-app/src/Screen/AddQuestion.js
+++ front-app/src/Screen/AddQuestion.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -7,94 +7,94 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 import Carasoul from "./Carasoul";
 
-export default function AddQuetion(){
+export default function AddQuetion() {
 
-    const [cus_id , setCusNo] = useState("");
-    const [cus_name , setName] = useState("");
-    const [cus_contact_no , setCusContactNo] = useState("");
-    const [question , setQuestion] = useState("");
+    const [cus_id, setCusNo] = useState("");
+    const [cus_name, setName] = useState("");
+    const [cus_contact_no, setCusContactNo] = useState("");
+    const [question, setQuestion] = useState("");
     const navigate = useNavigate();
-    
 
 
-    function sendData(event){
+
+    function sendData(event) {
 
         event.preventDefault();
 
-        const newQuestion ={
+        const newQuestion = {
             cus_id,
             cus_name,
             cus_contact_no,
             question
-            
-            
+
+
         }
 
-        
-        axios.post("http://localhost:8070/qna/addQuestion",newQuestion).then(()=>{
+
+        axios.post("http://localhost:8070/qna/addQuestion", newQuestion).then(() => {
             alert("Question submitted successfully!!!")
             navigate("/viewQuestion");
             window.location.reload(false);
-           
 
-        }).catch((err)=>{
+
+        }).catch((err) => {
             alert(err)
         })
 
     }
 
 
-    return(         
-       <div>
-        <Carasoul/>
-        <Header/>
-        <div className="container my-5 ">
-               <h1> Type here your question </h1>        
-           <Form className="rounded container my-5 mb-2 bg-dark text-white" style={{border:"1px solid black"}} onSubmit={sendData}>
-                
-               <br/>
-               <Form.Group className="mb-3" controlId="formCusId">
-                   <Form.Label >Customer ID</Form.Label>
-                   <Form.Control type="text" placeholder="C1234" required onChange={(event)=>{
-                           setCusNo(event.target.value);
-                       }}/>   
-               </Form.Group>
-   
-               <Form.Group className="mb-3" controlId="formCusName">
-                   <Form.Label >Your Name</Form.Label>
-                       <Form.Control type="text" placeholder="Perera A.B.C." required onChange={(event)=>{
-                           setName(event.target.value);
-                       }}/>
-               </Form.Group>
-   
-               <Form.Group className="mb-3" controlId="formCusCon">
-               <Form.Label>Contact No.</Form.Label>
-                       <Form.Control type="text" placeholder="071 234 567 8" required onChange={(event)=>{
-                           setCusContactNo(event.target.value);
-                       }}/>
-               </Form.Group>
-               
-                   
-                <Form.Group className="mb-3">
-                    <Form.Label for="exampleFormControlTextarea1"> Type your Question Here ...</Form.Label>
-                    <Form.Control as="textarea" rows={3} required onChange={(event)=>{
-                        setQuestion(event.target.value);
-                    }}/>
-                </Form.Group>
-            
-               <div>
-                    <td> 
-                        <Button variant="primary" size="sm" type="submit">Submit</Button>
-                    </td> 
-                   <td>
-                   <a href={`/viewQuestion`}>
-                      <Button type="button" variant="secondary" size="sm" class="btn btn-link">Cancel</Button>
-                   </a> 
-                   </td> 
-                   <br/>
-               </div>
-           </Form>
-       </div> 
-       </div>   
+    return (
+        <div>
+            <Carasoul />
+            <Header />
+            <div className="container my-5 ">
+                <h1> Type here your question </h1>
+                <Form className="rounded container my-5 mb-2 bg-dark text-white" style={{ border: "1px solid black" }} onSubmit={sendData}>
+
+                    <br />
+                    <Form.Group className="mb-3" controlId="formCusId">
+                        <Form.Label >Customer ID</Form.Label>
+                        <Form.Control type="text" placeholder="C1234" required onChange={(event) => {
+                            setCusNo(event.target.value);
+                        }} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formCusName">
+                        <Form.Label >Your Name</Form.Label>
+                        <Form.Control type="text" placeholder="Perera A.B.C." required onChange={(event) => {
+                            setName(event.target.value);
+                        }} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formCusCon">
+                        <Form.Label>Contact No.</Form.Label>
+                        <Form.Control type="text" placeholder="071 234 567 8" required onChange={(event) => {
+                            setCusContactNo(event.target.value);
+                        }} />
+                    </Form.Group>
+
+
+                    <Form.Group className="mb-3">
+                        <Form.Label for="exampleFormControlTextarea1"> Type your Question Here ...</Form.Label>
+                        <Form.Control as="textarea" rows={3} required onChange={(event) => {
+                            setQuestion(event.target.value);
+                        }} />
+                    </Form.Group>
+
+                    <div>
+                        <td>
+                            <Button variant="primary" size="sm" type="submit">Submit</Button>
+                        </td>
+                        <td>
+                            <a href={`/viewQuestion`}>
+                                <Button type="button" variant="secondary" size="sm" class="btn btn-link">Cancel</Button>
+                            </a>
+                        </td>
+                        <br />
+                    </div>
+                </Form>
+            </div>
+        </div>
     )
 }
